1aa4d3b9e0230fb8a4768417b7f45b6b9ec89d97
Jim Ritchey <jim0ritchey@gmail.com>
2019-03-05 21:39:34 -0500

    Added base game functionality

--- server.js
+++ server.js
@@ -1,41 +1,76 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const passport = require('passport');
-const routes = require('./routes');
-const session = require('express-session');
+var express = require('express');
+var app = express();
+var server = require('http').Server(app);
+var io = require('socket.io').listen(server);
+var players = {};
 
-const PORT = 4000;
+var star = {
+  x: Math.floor(Math.random() * 700) + 50,
+  y: Math.floor(Math.random() * 500) + 50
+};
+var scores = {
+  blue: 0,
+  red: 0
+};
 
-const app = express();
+app.use(express.static(__dirname + '/public'));
+ 
+app.get('/', function (req, res) {
+  res.sendFile(__dirname + '/client/index.html');
+});
 
+io.on('connection', function (socket) {
+  console.log('a user connected');
+  // create a new player and add it to our players object
+  players[socket.id] = {
+    rotation: 0,
+    x: Math.floor(Math.random() * 700) + 50,
+    y: Math.floor(Math.random() * 500) + 50,
+    playerId: socket.id,
+    team: (Math.floor(Math.random() * 2) == 0) ? 'red' : 'blue'
+  };
+  // send the players object to the new player
+  socket.emit('currentPlayers', players);
 
-const mongoDB = 'mongodb://127.0.0.1/project3';
-mongoose.connect(mongoDB, { useNewUrlParser: true });
-mongoose.Promise = global.Promise;
-const db = mongoose.connection;
+  // send the star object to the new player
+  socket.emit('starLocation', star);
+  // send the current scores
+  socket.emit('scoreUpdate', scores);
 
-app.use(session({
-  secret: 'ldjaisudnjdkalnasdfwpienlakjs',
-  resave: false, //required
-  saveUninitialized: false //required
-}));
+  // update all other players of the new player
+  socket.broadcast.emit('newPlayer', players[socket.id]);
 
-app.use(express.static('public'));
+  socket.on('disconnect', function () {
+    console.log('user disconnected');
+    // remove this player from our players object
+    delete players[socket.id];
+    // emit a message to all players to remove this player
+    io.emit('disconnect', socket.id);
+      });
 
-app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
-app.use(passport.initialize());
-app.use(passport.session());
-app.use(routes);
+    // when a player moves, update the player data
+    socket.on('playerMovement', function (movementData) {
+      players[socket.id].x = movementData.x;
+      players[socket.id].y = movementData.y;
+      players[socket.id].rotation = movementData.rotation;
+      // emit a message to all players about the player that moved
+      socket.broadcast.emit('playerMoved', players[socket.id]);
+    });
 
-db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+  socket.on('starCollected', function () {
+    if (players[socket.id].team === 'red') {
+      scores.red += 10;
+    } else {
+      scores.blue += 10;
+    }
+    star.x = Math.floor(Math.random() * 700) + 50;
+    star.y = Math.floor(Math.random() * 500) + 50;
+    io.emit('starLocation', star);
+    io.emit('scoreUpdate', scores);
+  });
 
-//Load in passport config
-require('./config/passport.js')(passport);
+});
 
-
-app.listen(PORT, (err) => {
-  if (err) throw err;
-
-  console.log(`Backend on port ${PORT}`);
+server.listen(8081, function () {
+  console.log(`Listening on ${server.address().port}`);
 });
