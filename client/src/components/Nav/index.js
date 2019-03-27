0666996ac385f4a6faaccda90b1e541fbfc62a21
Yobany <yobanydesign@gmail.com>
2019-03-27 18:33:59 -0400

    Added nav to game page

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -5,9 +5,13 @@ import './Nav.css';
 
 function Nav() {
     return (
-        <div className="side-nav">
-            <button><Link to={{ pathname: '/'}}>Home</Link></button>
-            <button><Link to={{ pathname: '/lobby'}}>Lobby</Link></button>
+        <div className="navContainer">
+            <div className="homeNav">
+            <button><Link to={{ pathname: '/'}}>Home       </Link><i class="material-icons">home</i></button>
+            </div>
+            <div className="lobbyNav">
+            <button><Link to={{ pathname: '/lobby'}}>Lobby      </Link><i class="material-icons">assignment</i></button>
+            </div>
         </div>
     )
 }
