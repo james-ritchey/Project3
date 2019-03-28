c50a48c6ac064198ac4ba8b5c584490ea6516f01
Yobany <yobanydesign@gmail.com>
2019-03-28 09:51:19 -0400

    High score game page tab

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -1,13 +1,22 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import HighScoreTable from "../HighScoreTable/index";
 
 import './Nav.css';
 
 function Nav() {
     return (
-        <div className="side-nav">
-            <button><Link to={{ pathname: '/'}}>Home</Link></button>
-            <button><Link to={{ pathname: '/lobby'}}>Lobby</Link></button>
+        <div className="navContainer">
+            <div className="homeNav">
+                <button><Link to={{ pathname: '/'}}>Home       </Link><i class="material-icons">home</i></button>
+            </div>
+            <div className="lobbyNav">
+                <button><Link to={{ pathname: '/lobby'}}>Lobby      </Link><i class="material-icons">assignment</i></button>
+            </div>
+            <div className="highScoreTab">
+                <HighScoreTable />
+            </div>
+
         </div>
     )
 }
